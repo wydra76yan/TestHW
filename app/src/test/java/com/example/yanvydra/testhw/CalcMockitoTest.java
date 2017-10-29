f6f77dea2b06d3ea4951f75d6e21b5d40911ac4b
wydra76yan <yan@vydra.com>
2017-10-29 15:08:26 +0300

    Captor, doReturn

--- app/src/test/java/com/example/yanvydra/testhw/CalcMockitoTest.java
+++ app/src/test/java/com/example/yanvydra/testhw/CalcMockitoTest.java
@@ -1,22 +1,29 @@
 package com.example.yanvydra.testhw;
 
-import static junit.framework.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Matchers;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.List;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CalcMockitoTest {
 
     private ICalc MCalc;
     private ICalc SCalc;
+
+    @Captor
+    ArgumentCaptor<Double> argumentCaptor;
 
     @Before
     public void setUp() {
@@ -39,11 +46,25 @@ public class CalcMockitoTest {
     @Test
     public void testAddSpy() {
         assertEquals(SCalc.add(4, 5), 9.0);
+
+        verify(SCalc).add(argumentCaptor.capture(),argumentCaptor.capture());
+
+        List<Double> value = argumentCaptor.getAllValues();
+        assertEquals(Double.valueOf(4), value.get(0));
+        assertEquals(Double.valueOf(5), value.get(1));
+    }
+
+    @Test
+    public void doRet(){
+        doReturn(null).when(MCalc).sub(0,1);
     }
 
     @Test
     public void testSubSpy() {
         assertEquals(SCalc.sub(56, 56), 0.0);
     }
+
+
+
 
 }
